6921ffb625853c35688a883ebbe12981a973b69b
mdo6180 <mdo6180@gmail.com>
2018-12-10 04:55:16 -0500

    created data.txt file according to LIBSVM format

--- small_pvalues.m
+++ small_pvalues.m
@@ -1,11 +1,23 @@
 clear
 close all
 
-data = readtable('CarcinomaNormalDataset.xls','Sheet','Cancer','Range','A1:AM13');
-data = removevars(data,{'Sample'});
-data(9,:) = [];
-data(end,:) = [];
-data = data(:,[3:end]);
-data = table2array(data(:,:));
-data = data';
+% read in presorted table
+% remove 'Sample column'
+% feature 'T87871' appears twice, so I am removing one of them
+% removing 'AccessionNumber' and 'Description' column
+% convert to array
+% transpose matrix so each row is an instance, each column is a feature
+data = readtable('CarcinomaNormalDataset.xls','Sheet','Cancer','Range','A1:AM12');
+data = removevars(data,{'Sample'});      
+data(9,:) = [];     
+data = data(:,[3:end]);     
+data = table2array(data(:,:));      
+features = data';   
 
+% creating class labels matrix, 
+% 1 = tumor, 0 = normal
+label_vector = zeros(36,1);
+label_vector(1:18) = 1;
+
+instance_matrix = sparse(features);     % converting features into sparse matrix
+libsvmwrite('data.txt', label_vector, instance_matrix);
