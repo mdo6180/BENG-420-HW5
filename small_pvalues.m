ab6de97a0ca80581827671daec7e24209703d8b3
mdo6180 <mdo6180@gmail.com>
2018-12-09 21:19:16 -0500

    added some more examples

--- small_pvalues.m
+++ small_pvalues.m
@@ -1,3 +1,11 @@
 clear
 close all
 
+data = readtable('CarcinomaNormalDataset.xls','Sheet','Cancer','Range','A1:AM13');
+data = removevars(data,{'Sample'});
+data(9,:) = [];
+data(end,:) = [];
+data = data(:,[3:end]);
+data = table2array(data(:,:));
+data = data';
+
